832b453276e6b74f387b99db90537904eb9fc3b7
Greg Poirier <greg.istehbest@gmail.com>
2025-08-17 11:51:04 -0400

    take comments out of ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -21,14 +21,11 @@ module.exports = {
       "repo": 'https://github.com/grepory/ragu.git',
       "path": '/Users/deploy/apps/ragu',
       "post-deploy": `
-        # Activate venv and install requirements
         source ../shared/venv/bin/activate &&
         pip install --upgrade pip &&
         pip install -r requirements.txt &&
-        # Create symlinks
         ln -sfn ../shared/chromadb ./chromadb &&
         ln -sfn ../shared/venv ./venv &&
-        # Copy env and restart
         cp ../.env .env &&
         /usr/local/bin/pm2 reload ecosystem.config.js --env production &&
         /usr/local/bin/node /Users/deploy/bin/update-caddy-config.js
